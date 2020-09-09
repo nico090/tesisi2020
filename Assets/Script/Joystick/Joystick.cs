eb9363531c33202fe8e68144536603ae7bd25005
Nicolas <nicolasfelix19@gmail.com>
2020-09-09 20:09:16 -0300

    Joystick
    Joystick

--- Assets/Script/Joystick/Joystick.cs
+++ Assets/Script/Joystick/Joystick.cs
@@ -1,18 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
 
 public class Joystick : MonoBehaviour
 {
+
+    [SerializeField] private Canvas canvas;
+    [SerializeField]private float radio;
+    private Vector3 posinicial;
+
+    private Vector2 axis;
     // Start is called before the first frame update
     void Start()
     {
-        
+        posinicial = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector2 Axis()
+    {
+        return axis;
+
+    }
+    public float AxisHorizontal()
     {
         
+        return axis.x;
     }
+    
+    public float AxisVertical()
+    {
+        
+        return axis.y;
+    }
+   public void Drag()
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, Input.mousePosition,
+            canvas.worldCamera, out pos);
+
+       Vector3 newPos=  canvas.transform.TransformPoint(pos);
+       newPos.x = Mathf.Clamp(newPos.x,-radio,radio);
+       newPos.y = Mathf.Clamp(newPos.y,-radio,radio);
+       
+       transform.position= newPos-posinicial;
+       axis = newPos / radio;
+
+    }
+
+
+   public void Drop()
+   {
+       transform.position = posinicial;
+
+   }
 }
