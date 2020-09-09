4acee9f90ee5f1d586546d11a6a193ce6af1bdf5
AgustinFranceschini <62911280+agus2306@users.noreply.github.com>
2020-09-09 19:22:09 -0300

    Menu Pausa
    Agregue escena del menu de pausa, con la opcion de continuar el juego y salir

---
+++ Assets/Script/Botones.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botones : MonoBehaviour
+{
+    public GameObject menuPausa;
+    public GameObject botonPausa;
+    public GameObject botonContinuar;
+    public bool IsPaused;
+
+    public void Start()
+    {
+        menuPausa.SetActive(false);
+        IsPaused = false;
+        Time.timeScale = 1;
+    }
+    public void PausarJuego()
+    {
+        menuPausa.SetActive(true);
+        IsPaused = true;
+        Time.timeScale = 0;
+    }
+    public void ContinuarJuego()
+    {
+        menuPausa.SetActive(false);
+        IsPaused = false;
+        Time.timeScale = 1;
+    }
+    public void SalirDelJuego()
+    {
+        Application.Quit();
+        Debug.Log("Saliste del juego");
+    }
+}
