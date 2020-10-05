f7d60ee497b94954647d1bcc1058ec9cc34b81ea
Nicolas <nicolasfelix19@gmail.com>
2020-10-05 20:20:06 -0300

    AI nueva sin SO
    asdkasld

---
+++ Assets/Script/Entity/Zombie.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.XR.WSA;
+
+
+public class Zombie : Entity
+{     Player player;
+    [SerializeField]private Transform[] positions;
+
+    private float angle;
+    private Rigidbody2D rigidbodyEnem;
+    private RaycastHit2D hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        rigidbodyEnem = GetComponent<Rigidbody2D>();
+        StartCoroutine(Patrulla());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   private  IEnumerator Patrulla()
+   {
+      int takePosition = Random.Range(0, positions.Length);
+        do
+        {
+            transform.position = Vector3.Lerp(transform.position, positions[takePosition].transform.position,
+                velocity * Time.deltaTime);
+            yield return new WaitForFixedUpdate();
+
+        } while (Vector3.Distance(transform.position, positions[takePosition].transform.position) >=0.2f);
+
+        StartCoroutine(Localizar());
+        StopCoroutine(Patrulla());
+    }
+
+   private IEnumerator Localizar()
+   {
+       float count = 0;
+       do
+       {
+           count++;
+           var position = transform.position;
+           Vector3 direccion = player.transform.position - position;
+
+           direccion.Normalize();
+            
+           //Debug.Log(direccion);
+
+           angle = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg - 90f;
+            
+           rigidbodyEnem.rotation = angle;
+             
+           hit = Physics2D.Raycast(position, direccion,100f, 1<<9 | 1<<10 );
+           Debug.DrawRay(position, direccion, Color.red);
+          yield return new WaitForFixedUpdate();
+       } while (hit.collider.tag!=("Player")||count<=2);
+       
+       if (hit.collider.tag == "Player")
+       {
+           StartCoroutine(Attack());
+           StopCoroutine(Localizar());
+       }
+       else
+       {
+           StartCoroutine(Patrulla());
+           StopCoroutine(Localizar());
+           yield return new WaitForEndOfFrame();
+       }
+       
+       
+   }
+
+   private IEnumerator Attack()
+   {
+       do
+       {
+           transform.position = Vector3.Lerp(transform.position, player.transform.position,
+                      velocity * Time.deltaTime);
+                  
+                  yield return new WaitForFixedUpdate();
+           
+       }while(Vector3.Distance(transform.position,player.transform.position)>=.3f);
+
+
+
+   }
+   
+}
+
+
