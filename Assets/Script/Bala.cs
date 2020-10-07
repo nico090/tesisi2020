ff66f750c0e1429abdbaca6d72f3f2f8960ae1c4
FJFFH <59668794+FJFFH@users.noreply.github.com>
2020-10-07 19:17:54 -0300

    Arma

---
+++ Assets/Script/Bala.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public float tiempodevida = 0.5f;
+    void Start()
+    {
+        Invoke("Destruir", tiempodevida);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,speed,0);
+    }
+
+    void Destruir()
+    {
+        Destroy(gameObject);
+    }
+}
+
--- Assets/Script/DisparoPlayer.cs
+++ Assets/Script/DisparoPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DisparoPlayer : MonoBehaviour
 {
@@ -8,33 +9,55 @@ public class DisparoPlayer : MonoBehaviour
 
     public float cadenciaDisparo;
     
-    public float cadenciaDisparo2 = 0.5f;
+    public float fireRate = 0.5f;
 
-    public GameObject bala;
+    public GameObject balaMetralleta;
+    public GameObject balaPistola;
+    public GameObject balaEscopeta;
+
+    public int tipoDeArma = 1;
     
     // Start is called before the first frame update
     void Start()
     {
-       // cadenciaDisparo =+ cadenciaDisparo2;
-        
         cadenciaDisparo = Time.time;
-        
-        cadenciaDisparo =+ cadenciaDisparo2;
-       
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (disparando == true && cadenciaDisparo < Time.time)
+       // Contador();
+        
+        if (disparando == true )
         {
-            cadenciaDisparo = Time.time + cadenciaDisparo2;
-            
-            StartCoroutine(DisparandoPistola());
+           // cadenciaDisparo = Time.time + cadenciaDisparo2;
 
-          //  cadenciaDisparo = Time.time + cadenciaDisparo2;
+            StartCoroutine(DisparandoPistola());
+            
+           // Contador();
         }
+
+        if (Input.GetKeyDown("1"))
+        {
+            tipoDeArma = 1;
+
+            fireRate = 0.3f;
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            tipoDeArma = 2;  
+            
+            fireRate = 0.1f;
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            tipoDeArma = 3;   
+            
+            fireRate = 0.8f;
+        }
+        
+        
+        
     }
 
     public void Disparar()
@@ -44,16 +67,44 @@ public class DisparoPlayer : MonoBehaviour
 
     IEnumerator DisparandoPistola()
     {
-        //if (cadenciaDisparo < Time.time)
-        //{
-        Instantiate(bala, transform.position, transform.rotation);
+        Debug.Log("si");
         
-        
-            Debug.Log("Estoy disparando");
+        if (cadenciaDisparo < Time.time && tipoDeArma == 1)
+        {
+            cadenciaDisparo = Time.time + fireRate;
+            Instantiate(balaPistola, transform.position, transform.rotation);
             
-        //}
+            
+          //  StopCoroutine("DisparandoPistola");
+        }
         
+        if (cadenciaDisparo < Time.time && tipoDeArma == 2) 
+        {
+            cadenciaDisparo = Time.time + fireRate;
+            Instantiate(balaMetralleta, transform.position, transform.rotation);
+        }
+        
+        if (cadenciaDisparo < Time.time && tipoDeArma == 3) 
+        {
+            cadenciaDisparo = Time.time + fireRate;
+            Instantiate(balaEscopeta, transform.position, transform.rotation);
+            Instantiate(balaEscopeta, transform.position, new Quaternion(transform.rotation.x,transform.rotation.y,transform.rotation.z + Random.Range(0.1f,0.01f), transform.rotation.w + Random.Range(0.1f,0.01f)));
+            Instantiate(balaEscopeta, transform.position, new Quaternion(transform.rotation.x,transform.rotation.y,transform.rotation.z - Random.Range(0.1f,0.01f), transform.rotation.w - Random.Range(0.1f,0.01f)));
+        }
+        /*
+
+        if (cadenciaDisparo > 1.01f)
+        {
+            cadenciaDisparo = 0;
+        }
+        */
 
         yield return null;
     }
+/*
+    public void Contador()
+    {
+        cadenciaDisparo += Time.deltaTime;
+    }
+    */
 }
